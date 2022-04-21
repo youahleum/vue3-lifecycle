2d5ac80a241096bf9bcb158e07392a6ff6328de0
youahleum <dkfma6033@naver.com>
2022-04-21 20:06:04 +0900

    vue3 practice

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- <h1 v-once @click="add">
+    {{ msg }}
+  </h1>
+  <h1 v-html="msg"></h1> -->
+  <Fruits />
+  <h1 :[attr]="'active'" @[event]="add">
+    {{ msg }}
+  </h1>
+</template>
+
+<script>
+import Fruits from "~/components/Fruits";
+// import Fruits from "./components/Fruits.vue";
+export default {
+  components: { Fruits },
+  data() {
+    {
+      Fruits;
+    }
+    return {
+      // msg: "<div style='color:red;'>hello!!</div>",
+      msg: "active",
+      attr: "class",
+      event: "click",
+    };
+  },
+  methods: {
+    add() {
+      this.msg += "!";
+    },
+  },
+};
+</script>
+<style scoped>
+.active {
+  color: red;
+  font-size: 100px;
+}
+</style>
---
+++ src/components/Fruits.vue
@@ -0,0 +1,75 @@
+<template>
+  <section v-if="hasFruits">
+    <h1>Fruits</h1>
+    <ul>
+      <li v-for="fruit in fruits" :key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+  </section>
+  <section>
+    <h1>Reverse Fruits</h1>
+    <ul>
+      <li v-for="fruit in reverseFruits" :key="fruit">
+        {{ fruit }}
+      </li>
+    </ul>
+  </section>
+  <section>
+    <button @click="add">ADD</button>
+    <!-- 매번 계산을 하는것 -->
+    <h1>{{ reverseMessage() }}</h1>
+
+    <!-- 계산된 데이터를 가져오는것 : computed : 캐싱의 기능이 있어 이렇게 가능 -->
+    <h1>{{ reversedMessage }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //Getter
+      //Setter : 값을 지정하는 용도
+
+      fruits: ["apple", "banana", "cherry"],
+      msg: "Hello Computed",
+    };
+  },
+  computed: {
+    hasFruits() {
+      return this.fruits.length > 0;
+    },
+
+    reverseFruits() {
+      return this.fruits.map((fruit) => {
+        return fruit.split("").reverse().join("");
+      });
+    },
+    //Getter : 값을 얻어내는 용도
+    // 읽기 전용 :  반응적으로 반응하지는 않는다
+    // reversedMessage() {
+    //   return this.msg.split("").reverse().join("");
+    // },
+
+    //Getter, Setter
+    reversedMessage: {
+      get() {
+        return this.msg.split("").reverse().join("");
+      },
+      set(newValue) {
+        this.msg = newValue;
+      },
+    },
+  },
+  methods: {
+    reverseMessage() {
+      return this.msg.split("").reverse().join("");
+    },
+    add() {
+      this.reversedMessage += "!?";
+      // this.msg += "!?";
+    },
+  },
+};
+</script>
